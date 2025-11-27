20505fb3839376ac25dffa079a76778ef999a61c
HasanAlizade007 <hesen00e7311@gmail.com>
2025-11-27 21:03:22 +0400

    LoginYenilik

---
+++ script.js
@@ -0,0 +1,43 @@
+// const num = Math.floor(Math.random() * 6) + 95;
+
+// const power5 = num ** 5;
+
+// const mod3 = num % 3;
+
+// console.log("Random ədəd (95-100):", num);
+// console.log("Ədədin 5-ci dərəcəsi:", power5);
+// console.log("3-ə bölünmədən qalan qalıq:", mod3);
+
+
+// let firstName = "Hasan"
+// let lastName = "Alizada"
+
+// console.log("Sizin adınız"+ " " + firstName+ " " + lastName + "." + "Tanışdığımıza şadam");
+
+
+// let bookPrice = 12;     
+// let bookAmount = 40;    
+
+// let totalValue = bookPrice * bookAmount;
+// console.log("Depodaki bütün kitabların ümumi dəyəri:", totalValue);
+
+//  const ad = prompt("Adınızı daxil edin:");
+//  console.log("Salam, " + ad + "!");
+
+// let a = Number(prompt("Birinci ədədi daxil edin:"));
+// let b = Number(prompt("İkinci ədədi daxil edin:"));
+
+// console.log("Nəticələr:");
+// console.log("Toplama: " + (a + b));
+// console.log("Çıxma: " + (a - b));
+// console.log("Vurma: " + (a * b));
+// console.log("Bölmə: " + (a / b));
+
+const a = 12345;
+const b = 98765;
+
+const hasil = a * b;
+const hasilStr = hasil.toString();
+
+console.log("Hasil:", hasil);
+console.log("6-cı rəqəm:", hasilStr[5]);
